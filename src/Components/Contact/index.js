4d2e0a6bde2c2b65ee5e539bd5968017533e0a7c
Pradeep-Selva <pradeepselvaraju@gmail.com>
2020-05-28 17:44:08 +0530

    contact form setup

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -17,6 +17,27 @@ const formStyle = classNames("formstyle", css(styles.bounceInUp))
 
 class Contact extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            full_name: "",
+            company_name: "",
+            email: "",
+            subject: "",
+            body: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        // this.sendMail = this.sendMail.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
     render() {
         return (
             <div className="form-page">
@@ -27,29 +48,29 @@ class Contact extends Component {
                         <form className="col s12" id="form" className={formStyle}>
                             <div className="row">
                                 <div className="input-field col s6">
-                                    <input id="full_name" type="text" className="validate" />
+                                    <input id="full_name" name="full_name" type="text" className="validate" onChange={this.handleChange} />
                                     <label for="full_name">Full Name</label>
                                 </div>
                                 <div className="input-field col s6">
-                                    <input id="company_name" type="text" className="validate" />
+                                    <input id="company_name" name="company_name" type="text" className="validate" onChange={this.handleChange} />
                                     <label for="company_name">Company Name</label>
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="input-field col s12">
-                                    <input id="email" type="email" className="validate" />
+                                    <input id="email" name="email" type="email" className="validate" onChange={this.handleChange} />
                                     <label for="email">Email</label>
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="input-field col s12">
-                                    <input id="subject" type="text" className="validate" />
+                                    <input id="subject" name="subject" type="text" className="validate" onChange={this.handleChange} />
                                     <label for="subject">Mail subject</label>
                                 </div>
                             </div>
                             <div className="row">
                                 <div class="input-field col s12">
-                                    <textarea id="textarea" class="materialize-textarea"></textarea>
+                                    <textarea id="textarea" name="body" class="materialize-textarea" onChange={this.handleChange}></textarea>
                                     <label for="textarea">Mail content</label>
                                 </div>
                             </div>
