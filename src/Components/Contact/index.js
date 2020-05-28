89897c8b37a7b7b0925ff3c3c38347190d376de2
Pradeep-selva <pradeepselvaraju@gmail.com>
2020-05-28 19:12:32 +0530

    contact form compelete

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
     }
 })
 
-const formStyle = classNames("formstyle", css(styles.bounceInUp))
+const formStyle = classNames("formstyle black-text", css(styles.bounceInUp))
 
 class Contact extends Component {
 
@@ -25,11 +25,14 @@ class Contact extends Component {
             company_name: "",
             email: "",
             subject: "",
-            body: ""
+            body: "",
+            isSent: false,
+            error: '',
+            btnContent: "Send"
         }
 
         this.handleChange = this.handleChange.bind(this)
-        // this.sendMail = this.sendMail.bind(this)
+        this.sendMail = this.sendMail.bind(this)
     }
 
     handleChange(e) {
@@ -38,46 +41,105 @@ class Contact extends Component {
         })
     }
 
+    sendMail(e) {
+        this.setState({
+            btnContent: "Sending..."
+        })
+        const {
+            subject,
+            body,
+            full_name,
+            company_name,
+            email } = this.state;
+
+        const message = `<p><b><em>Name: </em></b> ${full_name}</p>
+                        <p><b><em>Company: </em></b> ${company_name}</p>
+                        <p><b><em>Email: </em></b> ${email}</p>
+                        <hr>
+                        <p>${body}</p>`;
+
+        window.Email.send({
+            SecureToken: process.env.REACT_APP_SECURE_TOKEN,
+            To: process.env.REACT_APP_MAIL_TO,
+            From: process.env.REACT_APP_MAIL_FROM,
+            Subject: subject,
+            Body: message
+        }).then(
+            message => this.setState({
+                isSent: true,
+                btnContent: 'Successful'
+            })
+        ).catch(
+            error => this.setState({
+                error: error
+            })
+        )
+
+        e.preventDefault();
+    }
+
     render() {
+        const { btnContent,
+            isSent,
+            error } = this.state
+
         return (
             <div className="form-page">
                 <Navbar />
                 <div className="container">
                     <h1>Connect</h1>
                     <div className="row">
-                        <form className="col s12" id="form" className={formStyle}>
-                            <div className="row">
-                                <div className="input-field col s6">
-                                    <input id="full_name" name="full_name" type="text" className="validate" onChange={this.handleChange} />
-                                    <label for="full_name">Full Name</label>
-                                </div>
-                                <div className="input-field col s6">
-                                    <input id="company_name" name="company_name" type="text" className="validate" onChange={this.handleChange} />
-                                    <label for="company_name">Company Name</label>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="input-field col s12">
-                                    <input id="email" name="email" type="email" className="validate" onChange={this.handleChange} />
-                                    <label for="email">Email</label>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="input-field col s12">
-                                    <input id="subject" name="subject" type="text" className="validate" onChange={this.handleChange} />
-                                    <label for="subject">Mail subject</label>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div class="input-field col s12">
-                                    <textarea id="textarea" name="body" class="materialize-textarea" onChange={this.handleChange}></textarea>
-                                    <label for="textarea">Mail content</label>
-                                </div>
-                            </div>
-                        </form>
-                        <button form="form" type="submit" value="submit">
-                            Send
-                        </button>
+                        {
+                            !isSent && error == '' ?
+                                (
+                                    <form className="col s12" id="form" className={formStyle}>
+                                        <div className="row">
+                                            <div className="input-field col s6">
+                                                <input id="full_name" name="full_name" type="text" className="validate" onChange={this.handleChange} />
+                                                <label for="full_name">Full Name</label>
+                                            </div>
+                                            <div className="input-field col s6">
+                                                <input id="company_name" name="company_name" type="text" className="validate" onChange={this.handleChange} />
+                                                <label for="company_name">Company Name</label>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="input-field col s12">
+                                                <input id="email" name="email" type="email" className="validate" onChange={this.handleChange} />
+                                                <label for="email">Email</label>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="input-field col s12">
+                                                <input id="subject" name="subject" type="text" className="validate" onChange={this.handleChange} />
+                                                <label for="subject">Mail subject</label>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="input-field col s12">
+                                                <textarea id="textarea" name="body" class="materialize-textarea" onChange={this.handleChange}></textarea>
+                                                <label for="textarea">Mail content</label>
+                                            </div>
+                                        </div>
+                                        <button form="form" type="submit" value="submit" onClick={this.sendMail}>
+                                            {btnContent}
+                                        </button>
+                                    </form>
+
+                                ) :
+                                (
+
+                                    error == '' ?
+                                        (
+                                            <div className={css(styles.bounceInUp)}>
+                                                <h4>Mail Sent Successfully</h4>
+                                            </div>
+                                        ) :
+                                        <h4>{error}</h4>
+
+                                )
+                        }
+
                     </div>
                 </div>
 
