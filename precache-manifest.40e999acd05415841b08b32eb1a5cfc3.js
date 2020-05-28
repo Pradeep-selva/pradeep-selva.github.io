6020a3cd5960944a8d6c47073ebf65ca4ae23b1f
Pradeep-selva <pradeepselvaraju@gmail.com>
2020-05-28 20:46:54 +0530

    Updates

---
+++ precache-manifest.40e999acd05415841b08b32eb1a5cfc3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8eb2a286322cbc30d1fa61495f8ce6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ac88282d68f862d5398",
+    "url": "/static/css/2.4bcb7e64.chunk.css"
+  },
+  {
+    "revision": "377b99560d0995445fc1",
+    "url": "/static/css/main.674a4e8d.chunk.css"
+  },
+  {
+    "revision": "5ac88282d68f862d5398",
+    "url": "/static/js/2.269ba8d4.chunk.js"
+  },
+  {
+    "revision": "55d911ebaaf6a30305eeec12f4cc6e02",
+    "url": "/static/js/2.269ba8d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377b99560d0995445fc1",
+    "url": "/static/js/main.ce1b1add.chunk.js"
+  },
+  {
+    "revision": "a8f97462f4135fc3941b",
+    "url": "/static/js/runtime-main.39142cf7.js"
+  },
+  {
+    "revision": "7d776870e8784435475487e4026697ff",
+    "url": "/static/media/avatar.7d776870.jpg"
+  },
+  {
+    "revision": "a9d80d133eb92b4c1d40e4ad7b38fb6b",
+    "url": "/static/media/background.a9d80d13.png"
+  },
+  {
+    "revision": "66eebd71e569e755c68c1136c70fb807",
+    "url": "/static/media/facedetect.66eebd71.png"
+  },
+  {
+    "revision": "9e47750b29f79cff6760c9e6abb9f0fc",
+    "url": "/static/media/hacker-news.9e47750b.png"
+  },
+  {
+    "revision": "43ef46f25e109513d0c3b61d79593863",
+    "url": "/static/media/pokemon.43ef46f2.png"
+  },
+  {
+    "revision": "6919d7de053bc54ccfe96d69848a04e8",
+    "url": "/static/media/pong.6919d7de.png"
+  },
+  {
+    "revision": "f426975700ab1d160b2250f14b8d869c",
+    "url": "/static/media/weather.f4269757.png"
+  },
+  {
+    "revision": "d90e3b473807347be629e1e62dc363b4",
+    "url": "/static/media/yelp.d90e3b47.png"
+  }
+]);
