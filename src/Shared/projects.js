53ee7a959bd8c74f1d3665f6122de0687b9cf52a
Pradeep-selva <pradeepselvaraju@gmail.com>
2020-09-07 20:26:45 +0530

    converted jpegs to webp

--- src/Shared/projects.js
+++ src/Shared/projects.js
@@ -1,14 +1,14 @@
 export const Project = {
-  arcadia: require("../Assets/Projects/arcadia.jpg"),
-  connecsoc: require("../Assets/Projects/connecsoc.jpg"),
-  connecsocMob: require("../Assets/Projects/connecsoc_phone.jpg"),
-  connecsocTab: require("../Assets/Projects/connecsoc_tab.jpg"),
+  arcadia: require("../Assets/Projects/arcadia.webp"),
+  connecsoc: require("../Assets/Projects/connecsoc.webp"),
+  connecsocMob: require("../Assets/Projects/connecsoc_phone.webp"),
+  connecsocTab: require("../Assets/Projects/connecsoc_tab.webp"),
   facedetect: require("../Assets/Projects/facedetect.png"),
-  readersInv: require("../Assets/Projects/readers.jpg"),
-  readersInvMob: require("../Assets/Projects/readers_phone.jpg"),
-  readersInvTab: require("../Assets/Projects/readers_tab.jpg"),
+  readersInv: require("../Assets/Projects/readers.webp"),
+  readersInvMob: require("../Assets/Projects/readers_phone.webp"),
+  readersInvTab: require("../Assets/Projects/readers_tab.webp"),
   pokemon: require("../Assets/Projects/pokemon.png"),
-  yelp: require("../Assets/Projects/yelp.jpg"),
-  yelpMob: require("../Assets/Projects/yelp_phone.jpg"),
-  yelpTab: require("../Assets/Projects/yelp_tab.jpg")
+  yelp: require("../Assets/Projects/yelp.webp"),
+  yelpMob: require("../Assets/Projects/yelp_phone.webp"),
+  yelpTab: require("../Assets/Projects/yelp_tab.webp")
 };
