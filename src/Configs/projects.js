5e46271cde0b0b420c9e289d3553d208b6250e65
pradeep-selva <pradeepselvaraju@gmail.com>
2021-03-01 22:29:13 +0530

    projects content changes

--- src/Configs/projects.js
+++ src/Configs/projects.js
@@ -2,19 +2,19 @@ import { Project } from "../Shared";
 
 export const projects = [
   {
-    title: "Connecsoc",
-    repoLink: "https://github.com/Pradeep-selva/connecsoc",
-    thumbnail: Project.connecsoc,
+    title: "Breaddit",
+    repoLink: "https://github.com/Pradeep-selva/breaddit",
+    thumbnail: Project.breaddit,
     description:
-      "A social media website powered by react, redux, firebase and typescript. Join the site, make an account and post things for the rest of the users to see.\n\n"
+      "Sign up/Log in and join this community of bakers in a reddit like platform, to share your thoughts on your favorite kinds of breads in subreaddits of your choice, and interact with them.\n"
   },
   {
-    title: "Yelp Camp",
-    repoLink: "https://github.com/Pradeep-selva/YelpCamp",
-    thumbnail: Project.yelp,
+    title: "Arcadia Typerace",
+    repoLink: "https://github.com/Pradeep-selva/arcadia-typerace",
+    thumbnail: Project.typerace,
     description:
-      "This is a website that allows you to browse through campgrounds registered by users.You can create, read, update and delete (CRUD) from the application. This application follows REST-ful routing.",
-    id: "yelp"
+      "A website to challenge your friends to a typing race, powered by Go, Vue and WebSockets. Join/Create a room and invite your friends to challenge them to a typing race. Find out who's faster! Good luck."
+    // id: "yelp"
   },
 
   {
@@ -22,28 +22,27 @@ export const projects = [
     repoLink: "https://github.com/Pradeep-selva/Arcadia-Bot",
     thumbnail: Project.arcadia,
     description:
-      "A server specific general purpose discord bot built with discord.py that has a userbase of over 1k people.\n\n\n"
+      "A server specific general purpose discord bot built with discord.py that has a userbase of over 1.5k people.\n\n\n"
   },
   {
-    title: "Readers Inventory",
-    repoLink: "https://github.com/Pradeep-selva/readers-inventory",
-    thumbnail: Project.readersInv,
+    title: "React GUI",
+    repoLink: "https://github.com/Pradeep-selva/react-gui",
+    thumbnail: Project.reactGUI,
     description:
-      "This a website where you can add details of your favorite books, and manage them. Stay organised with your book lists and always find something new to read as others add books"
+      "A blazingly fast way to bootstrap react components. Create your react components quickly & easily with a straightforward GUI\n\n"
   },
   {
-    title: "Face and Eye Detector",
-    repoLink: "https://github.com/Pradeep-selva/Face-and-eye-detector",
-    thumbnail: Project.facedetect,
+    title: "Filx",
+    repoLink: "https://github.com/Pradeep-selva/filx",
+    thumbnail: Project.filx,
     description:
-      "A program that can detect eyes and face from input provided by webcam in real time built using the OpenCV library of Python3.\n\n",
-    whiteText: true
+      "A simple and light cli tool to organize your files with short neat commands, built with rust.\n\n"
   },
   {
-    title: "Tensorflow Pokemon Predictor",
-    repoLink: "https://github.com/Pradeep-selva/TensorFlow-Pokemon-Predictor",
-    thumbnail: Project.pokemon,
+    title: "react-hide-on-scroll",
+    repoLink: "https://github.com/Pradeep-selva/react-hide-on-scroll",
+    thumbnail: Project.hideOnScroll,
     description:
-      "This is a machine learning program built using Tensorflow to predict whether a given pokemon is legendary or not.\n\n\n"
+      "A react component library (npm package) for hiding or displaying content on your website based on your needs with ease.\n\n\n"
   }
 ];
--- src/Shared/projects.js
+++ src/Shared/projects.js
@@ -1,14 +1,8 @@
 export const Project = {
   arcadia: require("../Assets/Projects/arcadia.webp"),
-  connecsoc: require("../Assets/Projects/connecsoc.webp"),
-  connecsocMob: require("../Assets/Projects/connecsoc_phone.webp"),
-  connecsocTab: require("../Assets/Projects/connecsoc_tab.webp"),
-  facedetect: require("../Assets/Projects/facedetect.png"),
-  readersInv: require("../Assets/Projects/readers.webp"),
-  readersInvMob: require("../Assets/Projects/readers_phone.webp"),
-  readersInvTab: require("../Assets/Projects/readers_tab.webp"),
-  pokemon: require("../Assets/Projects/pokemon.png"),
-  yelp: require("../Assets/Projects/yelp.webp"),
-  yelpMob: require("../Assets/Projects/yelp_phone.webp"),
-  yelpTab: require("../Assets/Projects/yelp_tab.webp")
+  breaddit: require("../Assets/Projects/breaddit.webp"),
+  filx: require("../Assets/Projects/filx_demo.gif"),
+  hideOnScroll: require("../Assets/Projects/hide-on-scroll.webp"),
+  reactGUI: require("../Assets/Projects/react-gui.gif"),
+  typerace: require("../Assets/Projects/typerace.webp")
 };
